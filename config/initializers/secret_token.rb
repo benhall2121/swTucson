5d56e4562cd76dc2ecb124f96273db8a1d263c9b
Ben Hall <benhall2121@yahoo.com>
2011-08-19 10:08:51 -0700

    benhall2121@yahoo.com used nifty generators to create a user sigin and authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwTucson::Application.config.secret_token = '5805ffd162d39edfa28fa2d756b7c23ad2739c9f442200d7317adb2de609b77c8ac474cd6b16e36f001edb8989584eaff03d0373499f3e5bbab071c7371d2b4e'
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+SwTucson::Application.routes.draw do
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
+
+  match 'signup' => 'users#new', :as => :signup
+
+  match 'logout' => 'sessions#destroy', :as => :logout
+
+  match 'login' => 'sessions#new', :as => :login
+
+  resources :sessions
+
+  resources :users
+  
+  root :to => "sessions#new"
+
+end
